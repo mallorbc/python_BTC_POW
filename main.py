2cb4e2306a71cc3edd43d1b064516428064216cf
Blake Mallory <blakecmallory@gmail.com>
2021-06-21 16:17:06 -0400

    tell which nouce was found

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ if __name__ == '__main__':
                     print(status)
                     exit_status = True
             if exit_status:
+                print("found nounce " + str(status))
                 break
             millions = high_bound/1000000
             print("Hashed " + str(np.round(millions)) + " million times")
