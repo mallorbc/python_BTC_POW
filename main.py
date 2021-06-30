4ef4113e30cca8b8f77ef39c08a10ed20b624d4a
Blake Mallory <blakecmallory@gmail.com>
2021-06-30 15:00:10 -0400

    made multiprocessing its own function

--- main.py
+++ main.py
@@ -39,6 +39,65 @@ def one_core(difficulty, base_message,process_size):
             print("Hashed " + str(int(million)) + " million times")
             print("Hashrate: " + str(hash_rate) + " MH/sec")
 
+def multiprocess_hashing(difficulty,base_message,process_size,number_of_processes):
+    counter = 0
+    #gets the difficulty
+    solution = set_difficulty(difficulty)
+    #used for multiprocessing
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    exit_status = False
+    while True:
+        jobs = []
+        #tracks time start
+        start = time.time()
+        for i in range(number_of_processes):
+            #chunks works for each core
+            low_bound = counter*process_size
+            high_bound = ((counter+1)*process_size)-1
+
+            counter = counter + 1
+            #creates and starts job
+            p = multiprocessing.Process(target=hash_process, args=(
+                low_bound, high_bound, base_message, difficulty, solution, return_dict))
+            jobs.append(p)
+            p.start()
+        #waits for each job to finish
+        for proc in jobs:
+            proc.join()
+            status = return_dict.values()[0]
+            #checks if a nounce was returned or not
+            if status != False and exit_status == False:
+                exit_status = True
+                final_status = status
+        #tracks the end time
+        end = time.time()
+        #prints the found nouce and the hashed data
+        if exit_status:
+            print("found nounce: " + base_message + " " + str(final_status))
+            message = base_message + " " + str(final_status)
+            message = str.encode(message)
+            hashed_message = hashlib.sha256(message).hexdigest()
+            print(hashed_message)
+            break
+
+        #status updates
+        if process_size*number_of_processes>=1000000000:
+            billions = high_bound/1000000000
+            print("Hashed " + str(np.round(billions)) + " billion times")
+            hashes = high_bound - low_bound
+            time_elasped = end-start
+            hashrate = calc_hashrate(hashes,number_of_processes,time_elasped)
+            print("Hashrate: " + str(hashrate) + " MH/sec")
+        else:
+            millions = high_bound/1000000
+            print("Hashed " + str(np.round(millions)) + " million times")
+            hashes = high_bound - low_bound
+            time_elasped = end-start
+            hashes = high_bound - low_bound
+            time_elasped = end-start
+            hashrate = calc_hashrate(hashes,number_of_processes,time_elasped)
+            print("Hashrate: " + str(hashrate) + " MH/sec")
 
 #calculates MH/sec
 def calc_hashrate(hashes,number_of_processes,time):
@@ -65,7 +124,17 @@ def hash_process(low_bound, high_bound, base_message, difficulty, solution, retu
     return return_dict
 
 #runs algorithm and takes args
-def main():
+def main(base_message="Hello world",process_size=7,number_of_processes=10,difficulty=7,mode_to_run=0):
+    #sets the number of hashes to do per process
+    process_size = pow(10,process_size)
+
+    if mode_to_run == 0:
+        one_core(difficulty, base_message,process_size)
+    else:
+        multiprocess_hashing(difficulty, base_message,process_size,number_of_processes)
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser("Tool to demonstrate POW")
     parser.add_argument("-mes","--message", type=str, default="Hello world",help="What message do you want to hash")
     parser.add_argument("-d","--diff", type=int, default=7,help="How many leading zeros required for a valid hash")
@@ -75,73 +144,8 @@ def main():
 
     args = parser.parse_args()
     process_size = args.proc_size
-    #sets the number of hashes to do per process
-    process_size = pow(10,process_size)
     base_message = args.message
     difficulty = args.diff
     number_of_processes = args.processes
     mode_to_run = args.mode
-    if mode_to_run != 0:
-        one_core(difficulty, base_message,process_size)
-    else:
-        counter = 0
-        #gets the difficulty
-        solution = set_difficulty(difficulty)
-        #used for multiprocessing
-        manager = multiprocessing.Manager()
-        return_dict = manager.dict()
-        exit_status = False
-        while True:
-            jobs = []
-            #tracks time start
-            start = time.time()
-            for i in range(number_of_processes):
-                #chunks works for each core
-                low_bound = counter*process_size
-                high_bound = ((counter+1)*process_size)-1
-
-                counter = counter + 1
-                #creates and starts job
-                p = multiprocessing.Process(target=hash_process, args=(
-                    low_bound, high_bound, base_message, difficulty, solution, return_dict))
-                jobs.append(p)
-                p.start()
-            #waits for each job to finish
-            for proc in jobs:
-                proc.join()
-                status = return_dict.values()[0]
-                #checks if a nounce was returned or not
-                if status != False and exit_status == False:
-                    exit_status = True
-                    final_status = status
-            #tracks the end time
-            end = time.time()
-            #prints the found nouce and the hashed data
-            if exit_status:
-                print("found nounce: " + base_message + " " + str(final_status))
-                message = base_message + " " + str(final_status)
-                message = str.encode(message)
-                hashed_message = hashlib.sha256(message).hexdigest()
-                print(hashed_message)
-                break
-
-            #status updates
-            if process_size*number_of_processes>=1000000000:
-                billions = high_bound/1000000000
-                print("Hashed " + str(np.round(billions)) + " billion times")
-                hashes = high_bound - low_bound
-                time_elasped = end-start
-                hashrate = calc_hashrate(hashes,number_of_processes,time_elasped)
-                print("Hashrate: " + str(hashrate) + " MH/sec")
-            else:
-                millions = high_bound/1000000
-                print("Hashed " + str(np.round(millions)) + " million times")
-                hashes = high_bound - low_bound
-                time_elasped = end-start
-                hashes = high_bound - low_bound
-                time_elasped = end-start
-                hashrate = calc_hashrate(hashes,number_of_processes,time_elasped)
-                print("Hashrate: " + str(hashrate) + " MH/sec")
-
-if __name__ == '__main__':
-    main()
+    main(base_message,process_size,number_of_processes,difficulty,mode_to_run)
